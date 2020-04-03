fc7d81e930dc4a59f7dd1eb53ba22f0afc45b099
naomichi-y <n.yamakita@gmail.com>
2020-04-04 03:01:32 +0900

    Add list

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <div id="app">
     <button @click="createNewTodo">Add Todo</button>
+    <ul>
+      <li v-for="todo in todos" :key="todo.id">
+        {{todo.name}} - {{todo.description}}
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -8,6 +13,7 @@
 import API, {  graphqlOperation } from '@aws-amplify/api';
 // eslint-disable-next-line
 import { createTodo } from "./graphql/mutations";
+import { listTodos } from './graphql/queries'
 /*
 Supported log levels for browser debugging:
 ERROR
@@ -23,12 +29,25 @@ window.LOG_LEVEL = 'VERBOSE';
 
 export default {
   name: 'app',
+  data() {
+    return {
+      todos: []
+    }
+  },
   methods :{
     async createNewTodo() {
       const todo = { name: "Todo Title" , description: "あれをやる" + Date() }
 
       await API.graphql(graphqlOperation(createTodo, { input: todo }))
-    }
+    },
+    async getData() {
+      const todoData = await API.graphql(graphqlOperation(listTodos))
+
+      this.todos.push(...this.todos, ...todoData.data.listTodos.items);
+    },
+  },
+  created(){
+    this.getData()
   }
 };
 </script>
